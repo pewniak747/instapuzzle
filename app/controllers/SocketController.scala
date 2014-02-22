7b8803356c05e9646ed8da97906445998ac5d719
Tomasz Pewiński <pewniak747@gmail.com>
2014-02-22 17:40:46 +0100

    simple events parsing

--- app/controllers/SocketController.scala
+++ app/controllers/SocketController.scala
@@ -3,16 +3,28 @@ package controllers
 import scala.concurrent.duration._
 import akka.util.Timeout
 import play.api.mvc._
+import play.api.libs.json._
 
 import socketio._
 import socketio.PacketTypes._
+
+class Event
+
+case object PlayerJoin extends Event
+
+case class PlayerJoined(id: String, name: String) extends Event
+
+case class PlayerLeft(id: String) extends Event
 
 object MySocketIOController extends SocketIOController {
 
   val clientTimeout = Timeout(10.seconds)
 
   def processMessage(sessionId: String, packet: Packet) {
-    enqueueEvent(sessionId, """{"name":"dupa","args":[{}]}""")
+    parseIncomingEvent(packet.data).map {
+      case PlayerJoin => broadcastEvent(PlayerJoined(sessionId, "tom"))
+      case _ => broadcastEvent(packet.data)
+    }
   }
 
   override def initSession = Action { implicit request =>
@@ -20,6 +32,40 @@ object MySocketIOController extends SocketIOController {
     System.err.println("Strating new session: " + sessionId)
     val t = clientTimeout.duration.toSeconds.toString
     Ok(sessionId + ":" + t + ":" + t +":websocket")
+  }
+
+  private
+
+  def parseIncomingEvent(data: String): Option[Event] = {
+    try {
+      val json = Json.parse(data)
+      val eventName = json \ "name"
+      val eventArgs = (json \ "args")
+      (eventName, eventArgs) match {
+        case (JsString("player:join"), _) => Some(PlayerJoin)
+        case _ => None
+      }
+    }
+    catch {
+      case _ => None
+    }
+  }
+
+  def broadcastEvent(event: Event) {
+    broadcastEvent(Json.toJson(event).toString)
+  }
+
+  implicit val eventWrites = new Writes[Event] {
+    def writes(event: Event) = event match {
+      case PlayerJoined(id, name) => Json.obj(
+        "name" -> "player:joined",
+        "args" -> Json.arr(Json.obj(
+          "id" -> id,
+          "name" -> name
+        ))
+      )
+      case _ => Json.obj()
+    }
   }
 
 }
