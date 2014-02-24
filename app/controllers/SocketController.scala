b10df6d57e2626f5a74bb76b62b0c55f92db20fa
Tomasz Pewiński <pewniak747@gmail.com>
2014-02-24 20:49:50 +0100

    game actor

--- app/controllers/SocketController.scala
+++ app/controllers/SocketController.scala
@@ -2,57 +2,30 @@ package controllers
 
 import scala.concurrent.duration._
 import akka.util.Timeout
+import akka.actor._
+import play.api.libs.concurrent.Akka
 import play.api.mvc._
 import play.api.libs.json._
+import play.api.Play.current
 
 import socketio._
 import socketio.PacketTypes._
 
+import models.GameActor
+
 class Event
 
-case object PlayerJoin extends Event
+case class PlayerJoin(sessionId: String) extends Event
 
 case class PlayerJoined(id: String, name: String) extends Event
 
+case class PlayerLeave(sessionId: String) extends Event
+
 case class PlayerLeft(id: String) extends Event
 
-object MySocketIOController extends SocketIOController {
-
-  val clientTimeout = Timeout(10.seconds)
-
-  def processMessage(sessionId: String, packet: Packet) {
-    parseIncomingEvent(packet.data).map {
-      case PlayerJoin => broadcastEvent(PlayerJoined(sessionId, "tom"))
-      case _ => broadcastEvent(packet.data)
-    }
-  }
-
-  override def initSession = Action { implicit request =>
-    val sessionId = java.util.UUID.randomUUID().toString
-    System.err.println("Strating new session: " + sessionId)
-    val t = clientTimeout.duration.toSeconds.toString
-    Ok(sessionId + ":" + t + ":" + t +":websocket")
-  }
-
-  private
-
-  def parseIncomingEvent(data: String): Option[Event] = {
-    try {
-      val json = Json.parse(data)
-      val eventName = json \ "name"
-      val eventArgs = (json \ "args")
-      (eventName, eventArgs) match {
-        case (JsString("player:join"), _) => Some(PlayerJoin)
-        case _ => None
-      }
-    }
-    catch {
-      case _ => None
-    }
-  }
-
-  def broadcastEvent(event: Event) {
-    broadcastEvent(Json.toJson(event).toString)
+class EventDispatcher(controller: SocketIOController) extends Actor with ActorLogging {
+  def receive = {
+    case event : Event => controller.broadcastEvent(Json.toJson(event).toString)
   }
 
   implicit val eventWrites = new Writes[Event] {
@@ -65,6 +38,45 @@ object MySocketIOController extends SocketIOController {
         ))
       )
       case _ => Json.obj()
+    }
+  }
+}
+
+object MySocketIOController extends SocketIOController {
+
+  val clientTimeout = Timeout(10.seconds)
+
+  val dispatcher = Akka.system.actorOf(Props(new EventDispatcher(this)))
+
+  val game = Akka.system.actorOf(Props[GameActor], name = "game")
+
+  def processMessage(sessionId: String, packet: Packet) {
+    parseIncomingEvent(sessionId, packet.data).map { event =>
+      game.tell(event, dispatcher)
+    }
+  }
+
+  override def initSession = Action { implicit request =>
+    val sessionId = java.util.UUID.randomUUID().toString
+    System.err.println("Strating new session: " + sessionId)
+    val t = clientTimeout.duration.toSeconds.toString
+    Ok(sessionId + ":" + t + ":" + t +":websocket")
+  }
+
+  private
+
+  def parseIncomingEvent(sessionId: String, data: String): Option[Event] = {
+    try {
+      val json = Json.parse(data)
+      val eventName = json \ "name"
+      val eventArgs = (json \ "args")
+      (eventName, eventArgs) match {
+        case (JsString("player:join"), _) => Some(PlayerJoin(sessionId))
+        case _ => None
+      }
+    }
+    catch {
+      case _ => None
     }
   }
 
---
+++ app/models/Game.scala
@@ -0,0 +1,30 @@
+package models
+
+import akka.actor._
+
+import scala.collection.mutable
+
+import controllers._
+
+case class Player(id: String, name: String)
+
+class GameActor extends Actor with ActorLogging {
+
+  var playersMap:  mutable.Map[String, Player] = mutable.Map.empty
+
+  def receive = {
+    case PlayerJoin(sessionId) => {
+      val id = sessionId
+      val name = "random name"
+      playersMap.put(sessionId, Player(id, name))
+      sender ! PlayerJoined(id, name)
+    }
+    case PlayerLeave(sessionId) => {
+      playersMap.remove(sessionId).map { player =>
+        sender ! PlayerLeft(player.id)
+      }
+    }
+    case _ => Nil
+  }
+
+}
