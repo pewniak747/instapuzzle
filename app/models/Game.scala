9f59e0d8d215d7994362eed6d3ceb5244c892a3e
Tomasz Pewiński <pewniak747@gmail.com>
2014-03-02 16:59:47 +0100

    shuffle the board after creating game

--- app/models/Game.scala
+++ app/models/Game.scala
@@ -16,6 +16,8 @@ class Game extends Actor with ActorLogging {
 
   var board = new Board("http://distilleryimage6.ak.instagram.com/6191e200971711e3bad01215527ad906_8.jpg", 5, 5)
 
+  board.shuffle
+
   def receive = {
 
     case PlayerJoin(sessionId) => {
