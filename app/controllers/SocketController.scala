b4f1f900745694e521a9499516cb4a67373ad154
Tomasz Pewiński <pewniak747@gmail.com>
2014-03-17 21:53:36 +0100

    GameManager responsible for creating new boards

--- app/controllers/SocketController.scala
+++ app/controllers/SocketController.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import scala.util.Random
 import scala.concurrent.duration._
 import akka.util.Timeout
 import akka.actor._
@@ -26,18 +27,55 @@ class EventDispatcher(controller: SocketIOController) extends Actor with ActorLo
   implicit val serializer = new Serializer
 }
 
+class GameManager(broadcast: ActorRef) extends Actor with ActorLogging {
+  val game = context.actorOf(Props(new Game(broadcast)), name = "game")
+
+  def receive = {
+    case BoardRequest => {
+      game ! BoardChange(newBoard)
+    }
+
+    case event@_ => game.forward(event)
+  }
+
+  override def preStart = self ! BoardRequest
+
+  private
+
+  val images = List(
+    Image("http://distilleryimage0.ak.instagram.com/da5da88ca91311e3b145120e7295bf20_8.jpg", "http://instagram.com/p/lZsC2anc1C"),
+    Image("http://distilleryimage8.ak.instagram.com/3af3b0c2a91111e38d6c129f0cea75bc_8.jpg", "http://instagram.com/p/lZp8eKK35z"),
+    Image("http://distilleryimage0.ak.instagram.com/0f69c3aca91511e3b2630e9c0aaa8964_8.jpg", "http://instagram.com/p/lZs_zHuaYP"),
+    Image("http://distilleryimage4.ak.instagram.com/1c298576a90d11e3b4de12ff50c9f474_8.jpg", "http://instagram.com/p/lZme-zAm-B"),
+    Image("http://distilleryimage6.ak.instagram.com/6191e200971711e3bad01215527ad906_8.jpg", "http://instagram.com/p/kewEAyOjTR"),
+    Image("http://distilleryimage7.ak.instagram.com/57a2ac147ed211e3962a12bf16838833_8.jpg", "http://instagram.com/p/jPQOf6u6ez"),
+    Image("http://distilleryimage3.ak.instagram.com/ccb35e56a6c111e38aca0e51ab9d14d3_8.jpg", "http://instagram.com/p/lSFbkQgr8N"),
+    Image("http://distilleryimage3.ak.instagram.com/992f8acca6c211e38f0d1262a4bedf57_8.jpg", "http://instagram.com/p/lSGAzqFN5C")
+  )
+
+  val sizes = List((5, 5), (6, 6), (8, 8), (10, 10))
+
+  def newBoard: Board = {
+    val image = Random.shuffle(images).head
+    val (width, height) = Random.shuffle(sizes).head
+    val board = new Board(image, width, height)
+    board.shuffle
+    board
+  }
+}
+
 object MySocketIOController extends SocketIOController {
 
   val clientTimeout = Timeout(10.seconds)
 
   val dispatcher = Akka.system.actorOf(Props(new EventDispatcher(this)))
 
-  val game = Akka.system.actorOf(Props(new Game(dispatcher)), name = "game")
+  val gameManager = Akka.system.actorOf(Props(new GameManager(dispatcher)))
 
   def processMessage(sessionId: String, packet: Packet) {
     parseIncomingEvent(sessionId, packet.data).map { event =>
       System.err.println("Incoming event: " + event.toString)
-      game.tell(event, wsMap(sessionId))
+      gameManager.tell(event, wsMap(sessionId))
     }
   }
 
@@ -56,7 +94,7 @@ object MySocketIOController extends SocketIOController {
     }.map {
       _ => {
         println("all done quit.")
-        game ! PlayerLeave(sessionId)
+        gameManager ! PlayerLeave(sessionId)
       }
     }
     wsMap(sessionId) ! EventOrNoop
--- app/models/Game.scala
+++ app/models/Game.scala
@@ -3,7 +3,6 @@ package models
 import akka.actor._
 import scala.collection.mutable
 import scala.concurrent.duration._
-import scala.util.Random
 import faker.Name
 
 import play.api.libs.concurrent.Execution.Implicits._
@@ -12,30 +11,19 @@ import controllers._
 
 case class Player(id: String, name: String)
 
-case object BoardChange
+case class BoardChange(board: Board)
+
+case object BoardRequest
 
 class Game(val broadcast: ActorRef) extends Actor with ActorLogging {
 
-  var playersMap:  mutable.Map[String, Player] = mutable.Map.empty
+  var playersMap: mutable.Map[String, Player] = mutable.Map.empty
 
-  var holders:   mutable.Map[Piece, Player]  = mutable.Map.empty
+  var holders: mutable.Map[Piece, Player] = mutable.Map.empty
 
-  val images = List(
-    Image("http://distilleryimage0.ak.instagram.com/da5da88ca91311e3b145120e7295bf20_8.jpg", "http://instagram.com/p/lZsC2anc1C"),
-    Image("http://distilleryimage8.ak.instagram.com/3af3b0c2a91111e38d6c129f0cea75bc_8.jpg", "http://instagram.com/p/lZp8eKK35z"),
-    Image("http://distilleryimage0.ak.instagram.com/0f69c3aca91511e3b2630e9c0aaa8964_8.jpg", "http://instagram.com/p/lZs_zHuaYP"),
-    Image("http://distilleryimage4.ak.instagram.com/1c298576a90d11e3b4de12ff50c9f474_8.jpg", "http://instagram.com/p/lZme-zAm-B"),
-    Image("http://distilleryimage6.ak.instagram.com/6191e200971711e3bad01215527ad906_8.jpg", "http://instagram.com/p/kewEAyOjTR"),
-    Image("http://distilleryimage7.ak.instagram.com/57a2ac147ed211e3962a12bf16838833_8.jpg", "http://instagram.com/p/jPQOf6u6ez"),
-    Image("http://distilleryimage3.ak.instagram.com/ccb35e56a6c111e38aca0e51ab9d14d3_8.jpg", "http://instagram.com/p/lSFbkQgr8N"),
-    Image("http://distilleryimage3.ak.instagram.com/992f8acca6c211e38f0d1262a4bedf57_8.jpg", "http://instagram.com/p/lSGAzqFN5C")
-  )
+  var board: Option[Board] = None
 
-  val sizes = List((5, 5), (6, 6), (8, 8), (10, 10))
-
-  var board: Option[Board] = Some(newBoard)
-
-  def receive = started
+  def receive = finished
 
   def started: Receive = players orElse {
     case PiecePickup(sessionId, pieceId) => {
@@ -72,7 +60,7 @@ class Game(val broadcast: ActorRef) extends Actor with ActorLogging {
                 holders.clear
                 broadcast ! BoardFinished()
                 context.system.scheduler.scheduleOnce(5.seconds) {
-                  self ! BoardChange
+                  context.parent ! BoardRequest
                 }
               }
             }
@@ -85,10 +73,10 @@ class Game(val broadcast: ActorRef) extends Actor with ActorLogging {
   }
 
   def finished: Receive = players orElse {
-    case BoardChange => {
+    case BoardChange(newBoard: Board) => {
       board = Some(newBoard)
       context.become(started)
-      broadcast ! BoardSynced(board.get)
+      broadcast ! BoardSynced(newBoard)
     }
   }
 
@@ -114,7 +102,9 @@ class Game(val broadcast: ActorRef) extends Actor with ActorLogging {
     }
 
     case BoardSync(sessionId) => {
-      sender ! BoardSynced(board.get)
+      board.map { board =>
+        sender ! BoardSynced(board)
+      }
     }
   }
 
@@ -125,13 +115,5 @@ class Game(val broadcast: ActorRef) extends Actor with ActorLogging {
   private def pieceHolder(piece: Piece) = holders.get(piece)
 
   private def holding(player: Player) = holders.map(_.swap).get(player)
-
-  private def newBoard: Board = {
-    val image = Random.shuffle(images).head
-    val (width, height) = Random.shuffle(sizes).head
-    val board = new Board(image, width, height)
-    board.shuffle
-    board
-  }
 
 }
